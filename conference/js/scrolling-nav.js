a8ea2a1d97f32e6bfc8e73cddccbc0d09ec7b824
absali6693 <absali6693@gmail.com>
2016-01-15 23:32:33 +0530

    nav-bar color changes in js

--- conference/js/scrolling-nav.js
+++ conference/js/scrolling-nav.js
@@ -2,8 +2,12 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-fixed-top").removeClass("white-color");
+        $(".navbar-fixed-top").addClass("black-color");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top").removeClass("black-color");
+        $(".navbar-fixed-top").addClass("white-color");
     }
 });
 
