d49f22d1303f8aa2bcab6281784a425f6c2635ed
ujjwaljoshi93 <ujjwaljoshi93@gmail.com>
2016-01-16 00:52:27 +0530

    Removed sensor param in map url , changed link from minified to main bootstrap.css , removed link for topics.js , changed collapse to tabs in facilities.

--- conference/assets/js/main.js
+++ conference/assets/js/main.js
@@ -106,7 +106,7 @@ smoothScroll.init();
 jQuery(function($) {
     // Asynchronously Load the map API 
     var script = document.createElement('script');
-    script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+    script.src = "http://maps.googleapis.com/maps/api/js?callback=initialize";
     document.body.appendChild(script);
 });
 
