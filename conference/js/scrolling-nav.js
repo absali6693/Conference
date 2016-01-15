0d79a7db27e3b3cd55556b3f09ef5208c6d01533
absali6693 <absali6693@gmail.com>
2016-01-15 23:39:48 +0530

    nav-bar color scheme

--- conference/js/scrolling-nav.js
+++ conference/js/scrolling-nav.js
@@ -2,12 +2,12 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
-        $(".navbar-fixed-top").removeClass("white-color");
+        $(".navbar-fixed-top").removeClass("transparent-color");
         $(".navbar-fixed-top").addClass("black-color");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
         $(".navbar-fixed-top").removeClass("black-color");
-        $(".navbar-fixed-top").addClass("white-color");
+        $(".navbar-fixed-top").addClass("transparent-color");
     }
 });
 
